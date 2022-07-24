c3c38ded4d54a45a5f8046b4e8ec6994e913fca4
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-07-24 19:53:20 +0200

    création chevron bas

---
+++ src/components/icons/chevronBas.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg
+    width="48"
+    height="48"
+    viewBox="0 0 48 48"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class="relative h-[24px] w-[24px]"
+    preserveAspectRatio="none"
+  >
+    <path
+      d="M37.7679 17.8947L23.8799 31.7795L9.99194 17.8947"
+      stroke="#111827"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    ></path>
+  </svg>
+</template>
