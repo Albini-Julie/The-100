7ffc1397741c54076215033ff3ca9f4f1de10b44
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-07-24 14:21:00 +0200

    mise en place firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,19 @@ import App from './App.vue'
 import router from './router'
 import './index.css'
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-app.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD1vdTNZiamlrWK-T3cyXJSEbZKLgUjAzI",
+    authDomain: "the-100-550c7.firebaseapp.com",
+    projectId: "the-100-550c7",
+    storageBucket: "the-100-550c7.appspot.com",
+    messagingSenderId: "893754597187",
+    appId: "1:893754597187:web:f96ad289b413f9ead799f6"
+  };
+
+  const appFirebase = initializeApp(firebaseConfig);
+
 const app = createApp(App)
 
 app.use(router)
