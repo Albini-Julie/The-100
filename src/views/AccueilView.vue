04b2cb6e6d4b5c9e282270718f1cab30edf995d8
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-07-25 17:28:16 +0200

    création page accueil

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -2,17 +2,51 @@
   <header>
     <HeaderComp image="../../public/accueil/accueil1.jpg" texte="La série apocalyptique"></HeaderComp>
   </header>
-  <body></body>
+  <body class="">
+    <div class="mx-2 flex h-max gap-3">
+      <img class="w-2/5 rounded-lg object-cover" src="/public/accueil/accueil2.jpg" alt="image radiation" />
+      <div class="w-3/5">
+        <p class="font-open-sans text-[14px]">
+          The 100 est une série télévisée américaine produite par Jason Rothenberg et directement inspirée des romans éponymes de Kass
+          Morgans.
+        </p>
+        <bouton class="mx-5 mt-5 text-[14px]">Bande annonce</bouton>
+      </div>
+    </div>
+
+    <div class="my-8 bg-gray-900 p-3">
+      <img class="rounded-lg" src="../../public/accueil/accueil3.jpg" alt="image vaisseau" />
+      <p class="mt-2 mb-5 font-open-sans text-[14px] text-white">
+        A la suite d'une catastrophe nucléaire sur Terre, un groupe d'humains partent dans l'espace dans un ''vaisseau'', l'Arche surlequel
+        une véritable communauté va s'installer. d'envoyer 100 adolescents criminels pour vérifier.
+      </p>
+      <boutonBlanc>Les saisons</boutonBlanc>
+    </div>
+
+    <div class="mx-2 mb-10 flex h-max gap-3">
+      <div class="w-3/5">
+        <p class="font-open-sans text-[14px]">
+          Au bout de 97 ans, la Terre devrait être à nouveau habitable: afin de s'en assurer, les dirigeants de l'Arche décide
+        </p>
+        <bouton class="mx-5 mt-5 text-[14px]">Les personnages</bouton>
+      </div>
+      <img class="w-2/5 rounded-lg object-cover" src="/public/accueil/accueil4.jpg" alt="image radiation" />
+    </div>
+  </body>
   <FooterComp />
 </template>
 
 <script>
 import HeaderComp from "../components/HeaderComp.vue";
 import FooterComp from "../components/FooterComp.vue";
+import bouton from "../components/bouton.vue";
+import boutonBlanc from "../components/boutonBlanc.vue";
 export default {
   components: {
     HeaderComp,
     FooterComp,
+    bouton,
+    boutonBlanc,
   },
 };
 </script>
