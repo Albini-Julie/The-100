6133d439e48a434842c487eebaf0c7540045b384
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-07-25 17:27:20 +0200

    création des boutons

---
+++ src/components/bouton.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex justify-center rounded-lg bg-red-700 p-3 text-[14px]">
+    <span class="font-open-sans font-semibold text-white"><slot /></span>
+    <chevronDroit />
+  </div>
+</template>
+
+<script>
+import chevronDroit from "../components/icons/chevronDroit.vue";
+export default {
+  components: {
+    chevronDroit,
+  },
+};
+</script>
---
+++ src/components/boutonBlanc.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex justify-center rounded-lg bg-white p-3">
+    <span class="font-open-sans font-semibold text-red-700"><slot /></span>
+    <chevronDroitRouge />
+  </div>
+</template>
+
+<script>
+import chevronDroitRouge from "../components/icons/chevronDroitRouge.vue";
+export default {
+  components: {
+    chevronDroitRouge,
+  },
+};
+</script>
