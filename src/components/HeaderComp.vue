55efe9f0e961e99579e9d5643ed9d72992979b7f
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-07-25 17:27:49 +0200

    correction de bug dans le menu

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,7 +1,7 @@
 <template>
-  <header>
+  <header class="mb-10">
     <img class="absolute mt-[-28px]" :src="image" alt="image série" />
-    <div class="relative">
+    <div class="relative z-30">
       <div class="flex justify-between p-2">
         <router-link to="/">
           <img class="h-[33px] w-[47px]" src="../components/icons/logo-blanc.svg" alt="logo blanc" />
@@ -11,39 +11,44 @@
           <span class="sr-only">Menu</span>
         </button>
       </div>
+      <div class="mx-20 rounded-md bg-white text-center opacity-70">
+        <h2 class="font-taviraj font-semibold opacity-100">The 100</h2>
+        <h2 class="font-taviraj font-semibold opacity-100">{{ texte }}</h2>
+        <chevronBas class="m-auto" />
+      </div>
 
       <div
         class="fixed inset-0 translate-x-full motion-safe:transition-transform motion-safe:duration-1000"
         :class="[menuOuvert ? 'translate-x-0' : '']"
       >
         <div class="flex justify-center bg-gray-900">
-          <ul class="sm:flex">
-            <div class="my-36">
+          <ul class="mb-40 sm:flex">
+            <div class="mt-20">
+              <div class="mx-20 rounded-md bg-white text-center opacity-70">
+                <h2 class="font-taviraj font-semibold opacity-100">The 100</h2>
+                <h2 class="font-taviraj font-semibold opacity-100">La série apocalyptique</h2>
+                <chevronBas class="m-auto" />
+              </div>
               <div class="flex items-end">
-                <li class="mt-5 font-open-sans text-white"><router-link to="/LesSaisons">Les saisons</router-link></li>
+                <li class="mt-10 font-open-sans text-white"><router-link to="/LesSaisons">Les saisons</router-link></li>
                 <chevronDroit />
               </div>
               <div class="flex items-end">
-                <li class="mt-5 font-open-sans text-white"><router-link to="/LesPersonnages">Les Personnages</router-link></li>
+                <li class="mt-10 font-open-sans text-white"><router-link to="/LesPersonnages">Les Personnages</router-link></li>
                 <chevronDroit />
               </div>
               <div class="flex items-end">
-                <li class="mt-5 font-open-sans text-white"><router-link to="/MonAvis">Mon avis</router-link></li>
+                <li class="mt-10 font-open-sans text-white"><router-link to="/MonAvis">Mon avis</router-link></li>
                 <chevronDroit />
               </div>
               <div class="flex items-end">
-                <li class="mt-5 font-open-sans text-white"><router-link to="/MentionsLegales">Mentions Légales</router-link></li>
+                <li class="mt-10 font-open-sans text-white"><router-link to="/MentionsLegales">Mentions Légales</router-link></li>
                 <chevronDroit />
               </div>
             </div>
-            <FooterComp />
           </ul>
         </div>
-      </div>
-      <div class="mx-20 rounded-md bg-white text-center opacity-70">
-        <h2 class="font-taviraj font-semibold opacity-100">The 100</h2>
-        <h2 class="font-taviraj font-semibold opacity-100">{{ texte }}</h2>
-        <chevronBas class="m-auto" />
+        <FooterComp />
       </div>
     </div>
   </header>
