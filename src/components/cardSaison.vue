7c1d288c368427d3ee74be8ccecd30b0a2ad9e69
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-07-25 22:34:51 +0200

    ajustement du design carte saison

--- src/components/cardSaison.vue
+++ src/components/cardSaison.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <div class="flex justify-center">
-      <img class="h-[412px] w-[275px] rounded-lg" :src="image" alt="affiche saison" />
+      <img class="mx-10 h-[412px] w-[275px] rounded-lg" :src="image" alt="affiche saison" />
     </div>
-    <div class="mx-8 text-center leading-loose">
-      <h2 class="mb-2 font-taviraj text-[25px] font-semibold">{{ saison }}</h2>
-      <h2 class="font-taviraj text-[14px] font-semibold opacity-100">{{ texte }}</h2>
-      <h2 class="font-taviraj text-[14px] font-semibold opacity-100">{{ texte2 }}</h2>
-      <h2 class="font-taviraj text-[14px] font-semibold opacity-100">{{ texte3 }}</h2>
+    <div class="mt-2 bg-gray-900">
+      <div class="text-center leading-loose text-white">
+        <h2 class="mb-2 font-taviraj text-[25px] font-semibold">{{ saison }}</h2>
+        <h2 class="font-taviraj text-[14px] font-semibold opacity-100">{{ texte }}</h2>
+        <h2 class="font-taviraj text-[14px] font-semibold opacity-100">{{ texte2 }}</h2>
+        <h2 class="font-taviraj text-[14px] font-semibold opacity-100">{{ texte3 }}</h2>
+      </div>
     </div>
   </div>
 </template>
