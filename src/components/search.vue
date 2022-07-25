4bf69af7616e43d69745af16b9a3809ca51c7862
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-07-25 22:34:25 +0200

    création barre de recherche

---
+++ src/components/search.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex items-center justify-between rounded-lg border-2 border-gray-900 p-2">
+    <p class="font-open-sans text-[14px]">Chercher un personnage...</p>
+    <loupe />
+  </div>
+</template>
+
+<script>
+import loupe from "../components/icons/loupe.vue";
+export default {
+  components: {
+    loupe,
+  },
+};
+</script>
