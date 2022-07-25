44445eb07f7c369635a3ed60868824a0253c530a
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-07-25 10:51:48 +0200

    création du footer 1/2

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="w-max bg-white">
+    <img class="h-[117px] w-[169px]" src="../components/icons/logo-noir.svg" alt="site site web" />
+    <div class="flex"></div>
+  </div>
+</template>
+
