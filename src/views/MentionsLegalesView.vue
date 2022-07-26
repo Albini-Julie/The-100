8dcf0b8d8daacb453874a2c82c2123decc5aaa6f
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-07-26 19:32:05 +0200

    création page mentions légales

--- src/views/MentionsLegalesView.vue
+++ src/views/MentionsLegalesView.vue
@@ -1,5 +1,122 @@
-<template></template>
+<template>
+  <header>
+    <HeaderComp image="/public/mentionslegales/mentionslegales1.jpg" texte="Les mentions légales" />
+  </header>
+
+  <p class="mx-10 text-center font-open-sans text-[14px]">Voici un apperçu des mentions légles de notre site.</p>
+
+  <div class="mx-2">
+    <h4 class="my-2 font-open-sans text-xl font-bold">Contact</h4>
+    <p class="font-open-sans">
+      Pour toute question liée au festival, merci de contacter l’adresse suivante : julie.albini@edu.univ-fcomte.fr
+    </p>
+    <br />
+    <p class="font-open-sans">Pour envoyer tout dossier de presse ou cd de 4 Place Tharradin, 25200 Montbéliard - France</p>
+    <br />
+    <p class="font-open-sans">Tél +33 (0)3 81 99 47 34</p>
+
+    <h4 class="my-2 font-open-sans text-xl font-bold">Éditeur</h4>
+    <p class="font-open-sans">COCOON PRODUCTIONS (Association Loi 1901) Siège social : 4 Place Tharradin, 25200 Montbéliard - France</p>
+    <p class="font-open-sans">Tel : +33 (0)3 81 99 47 34</p>
+    <p class="font-open-sans">Licences entrepreneur de spectacles : PLATESV-B-2022-190203 / PLATESV-B-2022-280598</p>
+
+    <h4 class="my-2 font-open-sans text-xl font-bold">Hébergement</h4>
+    <p class="font-open-sans">infomaniak.com</p>
+
+    <h4 class="my-2 font-open-sans text-xl font-bold">Développement du site</h4>
+    <p class="font-open-sans">Design/développement (back/front) : Julie Albini</p>
+
+    <h4 class="my-2 font-open-sans text-xl font-bold">Formalités de déclaration</h4>
+    <p class="font-open-sans">
+      Le présent site Internet et notamment le traitement automatisé de l’information ont fait l’objet d’une déclaration formelle auprès de
+      la Commission Nationale de l’Informatique et des Libertés (CNIL).
+    </p>
+    <br />
+    <p class="font-open-sans">
+      Pour plus d’informations, merci de prendre contact avec Cocoon Productions : T. +33 (0)3 81 99 47 34 / julie.albini@edu.univ-fcomte.fr
+    </p>
+
+    <h4 class="my-2 font-open-sans text-xl font-bold">Droits d’auteur</h4>
+    <p class="font-open-sans">
+      Ce site est propriété de l’association Cocoon Productions – la réservation du nom de domaine www.cocoonfestival.fr, a été effectuée
+      conformément aux dispositions de l’AFNIC.
+    </p>
+    <br />
+    <p class="font-open-sans">
+      L’ensemble de ce site relève de la législation française et internationale sur le droit d’auteur et la propriété industrielle. Tous
+      les droits de reproduction sont réservés, y compris les représentations iconographiques et photographiques. La reproduction de tout ou
+      partie de ce site sur un support quel qu’il soit, est formellement interdite sauf autorisation de Cocoon productions.
+    </p>
+    <br />
+    <p class="font-open-sans">
+      La reproduction des textes sur un support papier est autorisée sous réserve du respect des trois conditions suivantes :
+    </p>
+    <br />
+    <ul>
+      <li class="ml-10 font-open-sans">• gratuité de la diffusion ;</li>
+      <br />
+      <li class="ml-10 font-open-sans">• respect de l’intégrité des documents reproduits (pas de modification ni altération) ;</li>
+      <br />
+      <li class="ml-10 font-open-sans">
+        • la citation claire et lisible de la source sous la forme suivante: « Document issu du site Internet www.cocoonfestival.fr Cocoon »
+        Les droits de reproduction sont réservés et strictement limités.
+      </li>
+    </ul>
+
+    <h4 class="my-2 font-open-sans text-xl font-bold">Politique de confidentialité des données</h4>
+    <p class="font-open-sans">
+      L’association COCOON PRODUCTIONS traite les données personnelles avec grand respect. C’est-à-dire que toutes les informations
+      personnelles que vous avez partagées ou partagerez avec COCOON PRODUCTIONS seront conservées en toute sécurité. Il n’y a aucune vente
+      de données à une autre entreprise, qu’elle soit partenaire ou non. Ci-dessous, en savoir plus sur l’utilisation, mais aussi la
+      conservation de vos données personnelles.
+    </p>
+    <br />
+    <p class="font-open-sans">Informations nominatives</p>
+    <br />
+    <p class="font-open-sans">
+      Les informations que nous recueillons sont destinées à mieux vous connaître, ne seront en aucun cas transmises à des tiers (exception
+      faite des autorités dûment habilités en vertu de dispositions légales et règlementaires). Conformément à la loi « Informatique et
+      Libertés » du 6 janvier 1978, qui intègre le RGPD (Règlement Général européen sur la Protection des Données) depuis le 25 mai 2018,
+      vous disposez d’un droit d’accès, de modification, de rectification et de suppression des données qui vous concernent.
+    </p>
+    <br />
+    <p class="font-open-sans">
+      Pour l’exercer, il vous suffit d’envoyer un e-mail à info julie.albini@edu.univ-fcomte.fr pour objet : DONNEES PERSONNELLES
+    </p>
+
+    <h4 class="my-2 font-open-sans text-xl font-bold">Sécurité</h4>
+    <p class="font-open-sans">
+      Le site web utilise la technologie https, qui encrypte toutes les informations qui transitent entre vos appareils et le site internet.
+      Les certificats SSL vous assurent que les pages que vous visitez proviennent bien de notre site et permettent ainsi d’éviter la
+      fraude.
+    </p>
+    <br />
+    <p class="font-open-sans">Confidentialité des courriers électroniques</p>
+    <br />
+    <p class="font-open-sans">
+      Concernant tout courrier électronique adressé à COCOON PRODUCTIONS à travers ce site, merci de prendre note du fait que :
+    </p>
+    <br />
+    <ul>
+      <li class="ml-10 font-open-sans">• Le secret des correspondances transmises par le réseau Internet n’est pas garanti ;</li>
+      <br />
+      <li class="ml-10 font-open-sans">
+        • Les courriers électroniques sont conservés sur support informatique, au moins le temps nécessaire à l’administration pour fournir
+        une réponse ou pour traiter votre demande.
+      </li>
+    </ul>
+  </div>
+
+  <FooterComp class="mt-10" />
+</template>
 
 <script>
-export default {};
+import HeaderComp from "../components/HeaderComp.vue";
+import FooterComp from "../components/FooterComp.vue";
+export default {
+  components: {
+    HeaderComp,
+    FooterComp,
+  },
+};
 </script>
