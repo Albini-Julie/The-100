3699f4f3b75105f1781b60a9e0e1c37a99d857bb
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-07-26 18:49:14 +0200

    création page saison 2

---
+++ src/views/Saison2View.vue
@@ -0,0 +1,88 @@
+<template>
+  <header>
+    <HeaderComp image="/public/saison/saison2-1.jpg" texte="La saison 2" />
+  </header>
+
+  <p class="mx-10 text-center font-open-sans text-[14px]">
+    Ici vous trouverez une présentation rapide de la saison 2, ainsi que les personnages importants qui apparaissent lors de cette saison.
+  </p>
+
+  <div class="mx-2 my-5 flex gap-2">
+    <img class="w-1/2 rounded-lg object-cover" src="/public/saison/saison2-2.jpg" alt="image scène de la série" />
+    <p class="w-1/2 font-open-sans text-[14px]">
+      La saison 2 est la découverte pour les 100 d'un nouveau clan : le clan de la montagne qui va les séparer et leur donner du fil à
+      retordre.
+    </p>
+  </div>
+  <p class="mx-2 font-open-sans text-[14px]">
+    Cette saison est aussi caractérisée par l'arrivée des habitants de l'Arche qui les rejoingnent sur Terre.
+  </p>
+
+  <cardPresentG
+    image="/public/lespersonnages/lespersonnages14.jpg"
+    titre="Lexa par Alycia Debnam-Carey"
+    texte1="C'est la commandante des Terriens."
+    texte2="Elle est très puissante et très forte au combat. Elle va évoluer fortement au côté de Clarke qui va également évoluer à ses côtés."
+    class="mt-5"
+  />
+  <cardPresentD
+    image="/public/lespersonnages/lespersonnages15.jpg"
+    titre="Maya Vie par Eve Harlow"
+    texte1="Elle va beaucoup se rapprocher de Jasper."
+    texte2="Elle fait partie du clan des montagnes mais trahie son clan en aidant Clarke et ses amis à s'échapper car elle est contre les actes dis de survie pratiqués par son clan."
+    class="mt-5"
+  />
+
+  <cardPresentG
+    image="/public/lespersonnages/lespersonnages16.jpg"
+    titre="Dante Wallace par Raymond John Barry"
+    texte1="C'est le président du clan des montagnes."
+    texte2="Il accueille à bras ouvert les 100, mais se fait trahir par son fils qui va créer une véritable guerre."
+    class="mt-5"
+  />
+  <cardPresentD
+    image="/public/lespersonnages/lespersonnages17.jpg"
+    titre="Cage Wallace par Johnny Whitworth"
+    texte1="C'est le fils du président des montagnards."
+    texte2="Il est contre l'accueil dans 100 dans le bucker et va organiser un coup d'état pour prendre le pouvoir de son père."
+    class="mt-5"
+  />
+
+  <cardPresentG
+    image="/public/lespersonnages/lespersonnages18.jpg"
+    titre="Indra Adina Porter"
+    texte1="C'est une terrienne faisant parti du clan des Wood."
+    texte2="Elle fait parti des hauts placés : elle conseille les commandants et prend souvent en charge les plans de guerre. Elle va beaucoup se rapprocher de Octavia et la considérer comme sa fille.
+"
+    class="mt-5"
+  />
+  <cardPresentD
+    image="/public/lespersonnages/lespersonnages19.jpg"
+    titre="Emori par Luisa D'Oliveira"
+    texte1="Emori est une terrienne nomade et n'a pas vraiment de clan en raison de sa déformation à l'une de ses mains qui l'a fait rejetée par son peuple."
+    texte2="Emori est une terrienne nomade et n'a pas vraiment de clan en raison de sa déformation à l'une de ses mains qui l'a fait rejetée par son peuple."
+    class="mt-5"
+  />
+
+  <router-link to="LesPersonnages">
+    <bouton class="m-auto w-1/2">Les personnages</bouton>
+  </router-link>
+  <FooterComp class="mt-10" />
+</template>
+
+<script>
+import HeaderComp from "../components/HeaderComp.vue";
+import cardPresentG from "../components/cardPresentG.vue";
+import cardPresentD from "../components/cardPresentD.vue";
+import bouton from "../components/bouton.vue";
+import FooterComp from "../components/FooterComp.vue";
+export default {
+  components: {
+    HeaderComp,
+    cardPresentG,
+    cardPresentD,
+    bouton,
+    FooterComp,
+  },
+};
+</script>
