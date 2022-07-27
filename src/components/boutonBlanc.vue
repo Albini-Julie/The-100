9b694f50a8e75d2f31f6003be9486c0e60105928
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-07-27 17:03:12 +0200

    ajustement des tailles des typos

--- src/components/boutonBlanc.vue
+++ src/components/boutonBlanc.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center justify-center rounded-lg bg-white p-3">
-    <span class="font-open-sans text-[14px] font-semibold text-red-700 md:text-[20px] xl:text-[35px]"><slot /></span>
+    <span class="font-open-sans text-[14px] font-semibold text-red-700 md:text-[20px] xl:text-[25px]"><slot /></span>
     <chevronDroitRouge />
   </div>
 </template>
