7c73ffbfdbfaaf3628e38a40e12b0d0665359e0b
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-07-27 16:25:03 +0200

    création header ordi

---
+++ src/components/HeaderGrand.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <div class="flex items-center justify-around bg-white">
+      <img class="h-[71px] w-[102px] xl:h-[111px] xl:w-[142px]" src="../components/icons/logo-noir.svg" alt="logo site" />
+      <p class="font-open-sans text-[25px]">Les saisons</p>
+      <p class="font-open-sans text-[25px]">Les personnages</p>
+      <p class="font-open-sans text-[25px]">Mon avis</p>
+    </div>
+    <img :src="image" alt="image série" />
+  </header>
+</template>
+
+
+<script>
+export default {
+  components: {},
+  props: {
+    image: String,
+  },
+};
+</script>
