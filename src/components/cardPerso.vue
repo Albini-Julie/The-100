3314b6d6a74b9595f8878280c22e15853eb2c0ea
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-07-27 17:56:27 +0200

    ajustement taille typos

--- src/components/cardPerso.vue
+++ src/components/cardPerso.vue
@@ -2,15 +2,15 @@
   <div class="border-2 border-gray-900">
     <img class="" :src="image" alt="image personnage" />
     <div class="ml-1">
-      <p class="font-open-sans text-[14px]">Prénom : {{ prenom }}</p>
-      <p class="font-open-sans text-[14px]">Nom : {{ nom }}</p>
-      <p class="font-open-sans text-[14px]">Surnoms : {{ surnom }}</p>
-      <p class="font-open-sans text-[14px]">Qualité : {{ qualite }}</p>
-      <p class="font-open-sans text-[14px]">Défaut : {{ defaut }}</p>
-      <p class="font-open-sans text-[14px]">Saisons : {{ saisons }}</p>
-      <p class="font-open-sans text-[14px]">Par : {{ par }}</p>
+      <p class="font-open-sans text-[20px] xl:text-[25px]">Prénom : {{ prenom }}</p>
+      <p class="font-open-sans text-[20px] xl:text-[25px]">Nom : {{ nom }}</p>
+      <p class="font-open-sans text-[20px] xl:text-[25px]">Surnoms : {{ surnom }}</p>
+      <p class="font-open-sans text-[20px] xl:text-[25px]">Qualité : {{ qualite }}</p>
+      <p class="font-open-sans text-[20px] xl:text-[25px]">Défaut : {{ defaut }}</p>
+      <p class="font-open-sans text-[20px] xl:text-[25px]">Saisons : {{ saisons }}</p>
+      <p class="font-open-sans text-[20px] xl:text-[25px]">Par : {{ par }}</p>
     </div>
-    <div class="my-2 flex justify-center gap-5">
+    <div class="my-2 flex justify-center gap-10">
       <poubelle />
       <crayon />
     </div>
--- src/components/search.vue
+++ src/components/search.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="flex items-center justify-between rounded-lg border-2 border-gray-900 p-2">
-    <p class="font-open-sans text-[14px]">Chercher un personnage...</p>
-    <loupe />
+  <div class="flex justify-center">
+    <div class="flex w-1/2 items-center justify-between rounded-lg border-2 border-gray-900 p-2">
+      <p class="font-open-sans text-[14px] md:text-[20px] xl:text-[25px]">Chercher un personnage...</p>
+      <loupe />
+    </div>
   </div>
 </template>
 
