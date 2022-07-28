0333a7ed8c8c156fe428d0aa069d93ca4dfdffbd
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-07-28 11:42:58 +0200

    mise à jour de l'espacement des items

--- src/components/search.vue
+++ src/components/search.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex justify-center">
-    <div class="flex w-1/2 items-center justify-between rounded-lg border-2 border-gray-900 p-2">
+    <div class="flex items-center justify-between gap-10 rounded-lg border-2 border-gray-900 p-2 xl:gap-44">
       <p class="font-open-sans text-[14px] md:text-[20px] xl:text-[25px]">Chercher un personnage...</p>
       <loupe />
     </div>
